06ac56574faf89ed133b3c5cf786560c274fc7d0
Maria-Chacon <mariajchm1312@gmail.com>
2023-06-01 17:15:20 -0600

    commit

---
+++ ReproductorDeVideo/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.mycompany.reproductordevideo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+
+    opens com.mycompany.reproductordevideo to javafx.fxml;
+    exports com.mycompany.reproductordevideo;
+}
