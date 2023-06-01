572acaa50e02f6b46891073df6d27f034689e819
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-06-01 13:56:02 -0600

    changes

--- ReproductorDeVideo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.reproductordevideo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-    opens com.mycompany.reproductordevideo to javafx.fxml;
-    exports com.mycompany.reproductordevideo;
-    requires javafx.mediaEmpty;
-}
